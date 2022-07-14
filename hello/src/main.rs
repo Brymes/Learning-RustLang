eb857984b0e92a31a8ff28f9aebcc0f242db8a84
Ghost Mac <theghostmac@gmail.com>
2022-07-13 21:36:13 -0400

    command line arguments in rust | euclid's algorithm

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,7 +1,28 @@
+use std::str::FromStr;
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!");
-    greatest_common_divisor(10, 20);
+    let mut numbers = Vec::new();
+
+    for arg in env::args().skip(1) {
+        numbers.push(u64::from_str(&arg))
+        .expect("error parsing argument");
+    }
+
+    if numbers.len() == 0 {
+        eprintln!("Usage: gcd NUMBER ...");
+        std::process::exit(1);
+    }
+
+    let mut d = numbers[0];
+    for m in &numbers[1..] {
+        d = greatest_common_divisor(d, *m);
+    }
+
+    println!("The greatest common divisor of {:?} is {}", numbers, d);
 }
+
 
 // Euclid's algorithm
 
