8d597dc5677c52f467ec1d92745e392c008d24b2
MacBobby Chibuzor <theghostmac@gmail.com>
2022-09-28 17:40:14 +0100

    progress.md | tracking my progress

---
+++ progress.md
@@ -0,0 +1,6 @@
+## Progress with Learning Rust
+
+===========
+
+[] Day 1: [Learn Rust deeply one step after the other](#learn-rust-deeply-one-step-after-the-other)
+[] Day 2: [Text Processing in Rust](#text-processing-in-rust)
---
+++ rust-syntax/target/debug/deps/rust_syntax-59c86503c1249347.d
@@ -0,0 +1,5 @@
+c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-59c86503c1249347.rmeta: src\main.rs
+
+c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-59c86503c1249347.d: src\main.rs
+
+src\main.rs:
