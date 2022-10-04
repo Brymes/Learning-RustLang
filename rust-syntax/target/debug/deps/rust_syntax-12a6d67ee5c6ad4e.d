d689e1bb0c5ae381838bebfef78e3ba3762e5807
MacBobby Chibuzor <theghostmac@gmail.com>
2022-10-04 01:35:20 +0100

    deleted everything

--- rust-syntax/target/debug/deps/rust_syntax-12a6d67ee5c6ad4e.d
+++
@@ -1,5 +0,0 @@
-c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-12a6d67ee5c6ad4e.rmeta: src\main.rs
-
-c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-12a6d67ee5c6ad4e.d: src\main.rs
-
-src\main.rs:
--- rust-syntax/target/debug/deps/rust_syntax-59c86503c1249347.d
+++
@@ -1,5 +0,0 @@
-c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-59c86503c1249347.rmeta: src\main.rs
-
-c:\Users\My_Laptop\rust\Learning-RustLang\rust-syntax\target\debug\deps\rust_syntax-59c86503c1249347.d: src\main.rs
-
-src\main.rs:
