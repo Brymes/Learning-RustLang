2d09c1dbfb92091f9e51767b47ed481c2b3972c5
MacBobby Chibuzor <theghostmac@gmail.com>
2022-10-09 23:37:14 +0100

    reinstalled rust for windows

---
+++ hello-rust/target/debug/deps/hello_rust-cd74acb6331ddbf6.d
@@ -0,0 +1,5 @@
+C:\Users\My_Laptop\rust\Learning-RustLang\hello-rust\target\debug\deps\hello_rust-cd74acb6331ddbf6.exe: src\main.rs
+
+C:\Users\My_Laptop\rust\Learning-RustLang\hello-rust\target\debug\deps\hello_rust-cd74acb6331ddbf6.d: src\main.rs
+
+src\main.rs:
---
+++ hello-rust/target/debug/hello-rust.d
@@ -0,0 +1,1 @@
+C:\Users\My_Laptop\rust\Learning-RustLang\hello-rust\target\debug\hello-rust.exe: C:\Users\My_Laptop\rust\Learning-RustLang\hello-rust\src\main.rs
