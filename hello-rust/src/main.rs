e5a3309fc30739a11802e3a0e3e30b4c6aecc4a7
MacBobby Chibuzor <theghostmac@gmail.com>
2022-10-14 06:31:43 +0100

    review

--- hello-rust/src/main.rs
+++ hello-rust/src/main.rs
@@ -1,11 +1,11 @@
-fn data_types() {
+fn main() {
     // variables:
     // mutable:
     let mut mutable_num: i64 = 100;
     // immutable:
     let my_immutable_num: i64 = 50;
     // string
-    let name = "MacBobby";
+    let name:&str = "MacBobby";
     // constants
     const THREE_HOURS_IN_SECONDS: u64 = 60 * 60 * 3;
     // Shadowing
@@ -16,7 +16,8 @@ fn data_types() {
         println!("The value of a in the inner scope is: {a}");
     }
     println!{"The value of the outer a is {a}"};
-    // Data Types: scalar and compound data types
+
+    // DATA Types: scalar and compound data types
     // scalar represent single value: integers, floating point numbers, booleans,characters.
     // float
     let fpn = 2.0; // f64
@@ -26,23 +27,31 @@ fn data_types() {
     let heart_eyed_cat = '😻';
 
 
-    // compound types are multiple values in a group: tuples and arrays
-    //tuple
+    // COMPOUND types are multiple values in a group: tuples and arrays
+    // tuple
     let tuppy: (i32, f64, u8) = (500, 12.2, 1);
-    // get individual values
+    // use pattern matching get individual values
     let (m, n, o) = tuppy;
-    printtln("The value of ");
-
+    println!("The value of n is: {n}");
+    // also use the dot notation
+    let five_hundred = tuppy.0;
+    let six_point_four = tuppy.1;
+    let one = tuppy.2;
+    // array: stores data on the stack not heap
+    let a = [1, 2, 3, 4, 5];
+    let months = ["January", "February", "December"];
+    // writing the type annotation
+    let my_array: [i64; 5] = [6, 7, 8, 9, 10];
+    // initializing
+    let another_array = [3; 5]; // let array = [3,3,3,3,3]
+    // accessing array elements
+    let first_element = my_array[0];
+    let second_element = my_array[1];
 
     println!("Hello!, my name is {}. My mutable is {}, and my immutable is {}", name, mutable_num, my_immutable_num);
     // another way to use formatting
     println!("Goodbye, now. It's {name}. And the time taken is {}", THREE_HOURS_IN_SECONDS);
-}
 
-fn main() {
-    let c = 'z';
-    let z: char = 'ℤ'; // with explicit type annotation
-    let heart_eyed_cat = '😻';
 
     println!("Hello!, my name is {}. My mutable is {}, and my immutable is {}", name, mutable_num, my_immutable_num);
     // another way to use formatting
---
+++ hello-rust/src/syntax.rs
@@ -0,0 +1,8 @@
+fn syntax() {
+    // variable declaration
+    let mut a: i16 = 10;
+    let b = "Good morning";
+    // data structures elementary
+    let my_array = [10, 20, 30];
+    let my_tuple = (500, 6.4, 1, "fifteen");
+}
