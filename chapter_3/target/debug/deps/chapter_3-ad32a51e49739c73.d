946fdde039fd0b6ba920426701c01b7471fa712a
MacBobby Chibuzor <theghostmac@gmail.com>
2022-10-30 07:16:03 +0100

    build deps

---
+++ chapter_3/target/debug/deps/chapter_3-ad32a51e49739c73.d
@@ -0,0 +1,5 @@
+/home/macbobby/workspace/Learning-RustLang/chapter_3/target/debug/deps/chapter_3-ad32a51e49739c73.rmeta: src/main.rs
+
+/home/macbobby/workspace/Learning-RustLang/chapter_3/target/debug/deps/chapter_3-ad32a51e49739c73.d: src/main.rs
+
+src/main.rs:
