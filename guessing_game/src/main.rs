6e20906d1ae9905375ab41117cda6a11cf89b431
MacBobby Chibuzor <theghostmac@gmail.com>
2022-11-08 21:21:22 +0100

    dissected rand library crate

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
 use std::io; // for obtaining input/output from user to console
+use rand::Rng;
 
 fn main() {
     println!("Guess the number!");
+
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100); // start..=end
+
+    println!("the secret number is: {secret_number}");
+
     println!("Please input your guess:");
     let mut guess = String::new(); // String is a growable UTF-8 encoded bit of text
 
