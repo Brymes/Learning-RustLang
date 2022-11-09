71e92d04babb467f705b67c024b027fa3e5f4431
MacBobby Chibuzor <theghostmac@gmail.com>
2022-11-09 22:38:07 +0100

    mutable refs

--- chapter_4/src/ref.rs
+++ chapter_4/src/ref.rs
@@ -1,10 +1,9 @@
 fn main() {
-    let s1 = String::from("hello");
-    let len = calculate_length(&s1);
+    let mut s = String::from("hello");
 
-    println!("the length of '{}' is {}.", s1, len);
+    change(&mut s);
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+fn change(some_string: &mut String) {
+    some_string.push_str(", world.");
 }
