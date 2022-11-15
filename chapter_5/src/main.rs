df96beea850a770d2f1313e7fc5bb183ad94f9ca
MacBobby Chibuzor <theghostmac@gmail.com>
2022-11-15 10:01:03 +0100

    5.1 structs done

--- chapter_5/src/main.rs
+++ chapter_5/src/main.rs
@@ -15,6 +15,37 @@ fn main() {
         email: String::from("theghostmac@gmaiil.com"),
         sign_in_count: 1,
     };
+    // access a single field in the struct:
     user1.email = String::from("macbobbychibuzor@gmail.com");
-    
+
+    // struct update
+    let user2 = User {
+        active: user1.active,
+        username: user1.username,
+        email: String::from("peekube01@gmail.com"),
+        sign_in_count: user1.sign_in_count,
+    };
+    let user3 = User {
+        email: String::from("alphamax.rdj@gmail.com"),
+        ..user1
+    };
+
+}
+
+// Tuple Structs
+    // Used to create nameless fields of different types
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+fn particle() {
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+}
+
+// Unit-Like Structs with no fields
+// useful in implementing a trait on some type but without a data in the type
+struct AlwaysEqual;
+
+fn status() {
+    let subject = AlwaysEqual;
 }
